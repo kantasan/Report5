f80d687c933f077a5c5550a3cbdef017b1edd5ad
Kawaguchi Kanta <e165701@ie.u-ryukyu.ac.jp>
2016-12-12 13:48:43 +0900

    report5

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * Created by e165701 on 2016/12/12.
+ */
+public class Main {
+    public static void main(String[]args){
+        String str = null;
+
+        System.out.println(str.length());
+     }
+}
+
